1eaf1df53b00d2ee0fb11571fe21eb2e091ede94
tsyonatanp <s0547274527@gmail.com>
2026-01-28 13:47:32 +0200

    Fix Google Analytics tracking

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,8 +1,23 @@
 'use client'
 
+import { usePathname, useSearchParams } from 'next/navigation'
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 export default function GoogleAnalytics({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_ID: string }) {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+
+    useEffect(() => {
+        const url = pathname + searchParams.toString()
+
+        if (typeof window !== 'undefined' && (window as any).gtag) {
+            (window as any).gtag('config', GA_MEASUREMENT_ID, {
+                page_path: url,
+            })
+        }
+    }, [pathname, searchParams, GA_MEASUREMENT_ID])
+
     return (
         <>
             <Script
